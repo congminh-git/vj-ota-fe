ac07ba7ebb15172f91a3ce1e44d34f1035b969bd
Cong Minh Tran <congminh0801@gmail.com>
2025-10-23 14:07:19 +0700

    fix route v10

--- src/app/dat-ve/thanh-toan/success/route.ts
+++ src/app/dat-ve/thanh-toan/success/route.ts
@@ -4,16 +4,8 @@ import { NextResponse } from 'next/server';
 export async function POST(req: Request) {
   const bodyText = await req.text();
 
-  // Giải mã form data nếu có
-  const params = new URLSearchParams(bodyText);
-  const orderId = params.get('orderId');
-  const transactionId = params.get('transactionId');
-  const status = params.get('status') || 'success';
+  console.log(bodyText)
 
-  // TODO: bạn có thể gọi API nội bộ, lưu DB, xác thực chữ ký thanh toán ở đây
-  // await fetch(`${process.env.API_URL}/save-transaction`, { method: 'POST', body: JSON.stringify({ orderId, transactionId }) })
-
-  // Redirect người dùng đến trang view React
   const redirectUrl = new URL(`/dat-ve/thanh-toan/success/view`, req.url);
   return NextResponse.redirect(redirectUrl, 303); // 303 = POST→GET redirect
 }
