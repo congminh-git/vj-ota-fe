179953e0daf6643227c81b629d833103a4e90645
Cong Minh Tran <congminh0801@gmail.com>
2025-10-23 15:32:39 +0700

    update route

--- src/app/dat-ve/thanh-toan/pending/route.ts
+++ src/app/dat-ve/thanh-toan/pending/route.ts
@@ -4,6 +4,6 @@ import { NextResponse } from 'next/server';
 export async function POST(req: Request) {
   const bodyText = await req.text();
 
-  const redirectUrl = new URL(`/dat-ve/thanh-toan/cancel/view`, req.url);
+  const redirectUrl = new URL(`/thanh-toan/cancel/view`, req.url);
   return NextResponse.redirect(redirectUrl, 303); // 303 = POST→GET redirect
 }
