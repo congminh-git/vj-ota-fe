104ca53cc1312970432202e684462c7bcfb70b6f
Cong Minh Tran <congminh0801@gmail.com>
2025-10-23 14:15:20 +0700

    fix route v13

--- src/app/dat-ve/thanh-toan/success/route.ts
+++ src/app/dat-ve/thanh-toan/success/route.ts
@@ -4,8 +4,8 @@ import { NextResponse } from 'next/server';
 export async function POST(req: Request) {
   const bodyText = await req.text();
 
-  console.log(bodyText)
+  const decoded:any = Buffer.from(bodyText, 'base64').toString('utf-8');
 
-  const redirectUrl = new URL(`/dat-ve/thanh-toan/success/view`, req.url);
+  const redirectUrl = new URL(`/dat-ve/thanh-toan/success/view?transactionID=${decoded?.responseData?.transactionID}`, req.url);
   return NextResponse.redirect(redirectUrl, 303); // 303 = POST→GET redirect
 }
