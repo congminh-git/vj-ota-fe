b28e6053a3661668a144598fc97c20a66f6d720b
Cong Minh Tran <congminh0801@gmail.com>
2025-10-23 11:46:43 +0700

    fix route v7

--- src/app/dat-ve/thanh-toan/success/route.ts
+++ src/app/dat-ve/thanh-toan/success/route.ts
@@ -4,6 +4,6 @@ export async function POST(req: Request) {
   const body = await req.text();
 
   // Redirect sang trang /dat-ve/thanh-toan/success/view với query
-  const redirectUrl = `/dat-ve/thanh-toan/success/view`;
-  return NextResponse.redirect(redirectUrl);
+  return NextResponse.redirect(new URL('/dat-ve/thanh-toan/success/view', req.url));
+
 }
