e4fd1d4cf9c3014c7838d9af8f35bb9a7f224b10
Cong Minh Tran <congminh0801@gmail.com>
2026-01-15 17:08:44 +0700

    remove docs

--- src/components/docs/heading.tsx
+++
@@ -1,6 +0,0 @@
-import { JSX } from "react"
-
-export const Heading = ({ id, level, children }: any) => {
-  const Tag = `h${level}` as keyof JSX.IntrinsicElements
-  return <Tag id={id}>{children}</Tag>
-}
